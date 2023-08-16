1c5481c2465c29652528a81af2ad613858ea695d
ethanchen1202 <17491128-ethanchen1202@users.noreply.replit.com>
2023-08-16 18:51:10 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+//api
+//let url =
+//fetch(url)
+
+//TRANSITIONS
+const navTransition = () => {
+  let burger = document.querySelector(".burger");
+  let nav = document.querySelector(".links");
+  let navLinks = document.querySelectorAll(".links li");
+
+  //toggling nav
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("linksActive")
+    //adding animation to links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `linksFade 400ms ease forwards ${index / 5 + 0.6}s`;
+      }
+    });
+    //adding animation to burger
+    burger.classList.toggle("toggle");
+  });
+}
+
+navTransition();
+
+//STICKY FUNCTION
+window.onscroll = function() { stickyTransition() };
+
+let navbar = document.querySelector("nav");
+let nav = document.querySelector(".links");
+let sticky = navbar.offsetTop;
+
+function stickyTransition() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+    nav.style.top = "8vh";
+  } else {
+    navbar.classList.remove("sticky");
+    nav.style.top = "15vh";
+  }
+}
